1689be5920112aa86dc45beb71042984878ed87f
Yusufa Haidar <haidardewa@gmail.com>
2024-03-07 13:45:04 +0700

    Pertemuan 4 - Praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,16 +14,28 @@ class UserController extends Controller
     //     -> with('nama', $nama);
     // }
 
-    public function index(){
-        $data = [
-            // 'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            // 'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+    // public function index(){
+    //     $data = [
+    //         // 'level_id' => 2,
+    //         'username' => 'manager_tiga',
+    //         'nama' => 'Manager 3',
+    //         // 'password' => Hash::make('12345')
+    //     ];
+    //     UserModel::create($data);
 
-        $user = UserModel::all();
+    //     $user = UserModel::all();
+    //     return view('user', ['data'=>$user]);
+    // }
+
+    // public function index(){
+    //     $user = UserModel::firstWhere('level_id', 1);
+    //     return view('user', ['data'=>$user]);
+    // }
+
+    public function index(){
+        $user = UserModel::findOr(20, ['username', 'nama'], function(){
+            abort(404);
+        });
         return view('user', ['data'=>$user]);
     }
 }
