7c0807d39db80705e9c11ecc3cd62acc4be6a495
Yusufa Haidar <haidardewa@gmail.com>
2024-05-16 13:55:39 +0700

    Pertemuan 10 - Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,11 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 
 class UserModel extends Authenticatable
 {
+
+    public function getJwtIdentifier(){
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(){
+        return [];
+    }
+
     use HasFactory;
 
     protected $table = 'm_user';
