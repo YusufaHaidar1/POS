40667b1ff0b4d488a7a40d0f5a9b675df6fd7b59
Yusufa Haidar <haidardewa@gmail.com>
2024-05-19 15:23:54 +0700

    Pertemuan 11 - Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Tymon\JWTAuth\Contracts\JWTSubject;
@@ -25,11 +26,14 @@ class UserModel extends Authenticatable implements JWTSubject
     protected $primaryKey = 'user_id';
     public $timestamps = false;
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password', 'image'];
 
     public function level()
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 
+    protected function image():Attribute{
+        return Attribute::make(get: fn ($image) => url('/storage/posts/' . $image));
+    }
 }
